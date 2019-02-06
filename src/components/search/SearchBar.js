236195f35dcfa09fdc6e45b6ccbbca1641307a65
Ashkaan Jaberi <ashkaan14@gmail.com>
2019-02-06 20:04:30 +0000

    basic nav outlne done

--- src/components/search/SearchBar.js
+++ src/components/search/SearchBar.js
@@ -5,22 +5,34 @@ class SearchBar extends Component{
   state = {
     query: '',
   }
+  
+  handleChange = (e) => {
+    this.setState({query: e.target.value});
+  }
+  
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state.query);
+    this.setState({query:''});
+  }
+  
   render(){
     return(
         <form 
           className='search-bar'
-          onSubmit={this.onSubmit}
+          onSubmit={this.handleSubmit}
         >
           <input
             className='search-input'
             type='text'
             value={this.state.query}
-            onChange={this.onChange}
+            onChange={this.handleChange}
             placeholder='Search books'
           />
         
           <input 
             className='cancel-button'
+            onSubmit={this.onSubmit}
             type='submit' 
             value='Cancel' 
           />
