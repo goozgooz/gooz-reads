cec7939a8a2847cfe12d318b08275758ba52aaa5
Ashkaan Jaberi <ashkaan14@gmail.com>
2019-02-08 16:55:04 +0000

    starting Lock version

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,6 +1,7 @@
 import './_app.scss';
 import React, { Component } from 'react';
-
+import Auth0Lock from 'auth0-lock';
+console.log(Auth0Lock);
 import Home from '../home/Home';
 
 class App extends Component {
